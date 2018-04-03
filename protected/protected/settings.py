a7024cd4b3700873d1c563e449fa0f5961675a92
Ben Timby <btimby@gmail.com>
2018-04-03 17:45:10 -0400

    Updated config.

--- protected/protected/settings.py
+++ protected/protected/settings.py
@@ -132,3 +132,29 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
     )
 }
+
+
+# Note, all default values are left commented out for documentation purposes.
+SIMPLE_JWT = {
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    # 'ROTATE_REFRESH_TOKENS': False,
+    # 'BLACKLIST_AFTER_ROTATION': True,
+
+    # TODO: must be synchronized with auth-service application.
+    'SIGNING_KEY': SECRET_KEY,
+
+    # 'ALGORITHM': 'HS256',
+    # 'VERIFYING_KEY': None,
+
+    # 'AUTH_HEADER_TYPES': ('Bearer',),
+    # 'USER_ID_FIELD': 'id',
+    # 'USER_ID_CLAIM': 'user_id',
+
+    # 'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    # 'TOKEN_TYPE_CLAIM': 'token_type',
+
+    # 'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    # 'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    # 'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
