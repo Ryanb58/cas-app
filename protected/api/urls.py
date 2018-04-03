3409dea5aa95e13ba3e0b70d1a417840c9725ff6
Ben Timby <btimby@gmail.com>
2018-04-03 18:11:34 -0400

    Issue tokens for Django users (for testing).

--- protected/api/urls.py
+++ protected/api/urls.py
@@ -2,9 +2,15 @@
 """
 from django.conf.urls import url, include
 
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView, TokenRefreshView
+)
+
 from api.views import Me
 
 
 urlpatterns = [
-    url('^me/', Me.as_view()),
+    url('^me/$', Me.as_view()),
+    url('^token/$', TokenObtainPairView.as_view()),
+    url('^token/refresh/$', TokenRefreshView.as_view()),
 ]
--- protected/api/views.py
+++ protected/api/views.py
@@ -4,9 +4,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
+from api.serializers import TokenUserSerializer
+
 
 class Me(APIView):
     permission_classes = (IsAuthenticated,)
+    serializer_classes = ()
 
     def get(self, request):
-        return Response(request.user)
+        return Response(TokenUserSerializer(request.user).data)
